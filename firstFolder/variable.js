18416be5b44859a5fe14c034194e1cfc954d5ab0
ayathree <nobanitaayathree333@gmail.com>
2025-03-23 00:35:21 +0600

    code

--- firstFolder/variable.js
+++ firstFolder/variable.js
@@ -1,5 +1,20 @@
-let a = 10;
-let b = 3;
-console.log(a + b); 
-console.log(a % b); 
-console.log(a-b);
+let name = "Pooja";
+let age = 25;
+let isStudent = true;
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isStudent, typeof isStudent)
+
+
+// 2
+
+const a = 10;
+const b = 5;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
